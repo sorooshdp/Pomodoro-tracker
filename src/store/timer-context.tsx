6cee0b57cb310653e4352c2c5390c87e35d183c2
soroosh <sorooshdpki@gmail.com>
2024-02-01 19:02:59 +0330

    feat: add ui-context and ui-context components
    these components store global data using react context API

---
+++ src/store/timer-context.tsx
@@ -0,0 +1,10 @@
+/**
+ * this component saves infomation related to timer: focus length, pomodoros until 
+ * long break, short break length, long break length, Auto resume timer, sound, notification
+ * and other timer related information 
+ */
+// const TimerContextProvider : React.FC = () => {
+
+// }
+
+export {};
